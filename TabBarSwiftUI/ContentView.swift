c87ae2330e3e5ab8cdc20db7d0e25c8c978d1cda
Ahmed Elzohry <aelzohry2010@gmail.com>
2019-10-26 07:57:01 +0200

    implement custom badge number for TabView [workaround]

--- TabBarSwiftUI/ContentView.swift
+++ TabBarSwiftUI/ContentView.swift
@@ -9,13 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+  @State private var badgeNumber: Int = 3
+  private var badgePosition: CGFloat = 3
+  private var tabsCount: CGFloat = 3
+  
+  var body: some View {
+    GeometryReader { geometry in
+      ZStack(alignment: .bottomLeading) {
+        // TabView
+        TabView {
+          Text("First View")
+            .tabItem {
+              Image(systemName: "1.circle")
+              Text("First")
+          }.tag(0)
+          
+          Text("Second View")
+            .tabItem {
+              Image(systemName: "2.circle")
+              Text("Second")
+          }.tag(1)
+          
+          Text("Third View")
+            .tabItem {
+              Image(systemName: "3.circle")
+              Text("Third")
+          }.tag(2)
+        }
+        
+        // Badge View
+        ZStack {
+          Circle()
+            .foregroundColor(.red)
+          
+          Text("\(self.badgeNumber)")
+            .foregroundColor(.white)
+            .font(Font.system(size: 12))
+        }
+        .frame(width: 20, height: 20)
+        .offset(x: ( ( 2 * self.badgePosition) - 1 ) * ( geometry.size.width / ( 2 * self.tabsCount ) ), y: -30)
+        .opacity(self.badgeNumber == 0 ? 0 : 1)
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    Group {
+      ContentView()
+            
+      ContentView().previewDevice("iPhone SE")
     }
+  }
 }
